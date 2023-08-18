4260be31e7bf20d416bfa039773e80d8e0af51aa
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-08-18 06:49:12 +0530

    another try JWT

---
+++ JWT3.0/src/main/java/com/config/MyConfig.java
@@ -0,0 +1,38 @@
+package com.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+public class MyConfig {
+	
+	
+	// when we will use database we will change this userdetailService class to our CustomUserService class
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user =  User.builder().username("abhinav").password(passwordEncoder().encode("1234")).roles("ADMIN").build();
+		return new InMemoryUserDetailsManager(user);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
+    }
+}
