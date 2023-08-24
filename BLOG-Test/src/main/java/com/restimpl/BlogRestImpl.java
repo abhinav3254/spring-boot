5860a187cf920d2435d10af44bc512ff668c567b
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-08-24 21:52:54 +0530

    delete and find blog by id

--- BLOG-Test/src/main/java/com/restimpl/BlogRestImpl.java
+++ BLOG-Test/src/main/java/com/restimpl/BlogRestImpl.java
@@ -1,5 +1,7 @@
 package com.restimpl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pojo.Blog;
 import com.rest.BlogRest;
 import com.service.BlogService;
 
@@ -26,4 +29,35 @@ public class BlogRestImpl implements BlogRest {
 		return new ResponseEntity<String>("Internal Issue in BlogRestImpl",HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
+	@Override
+	public ResponseEntity<List<Blog>> getAllBlog() {
+		try {
+			return blogService.getAllBlog();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<List<Blog>>(new ArrayList<Blog>(),HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@Override
+	public ResponseEntity<Blog> getBlogById(String id) {
+		try {
+			return blogService.getBlogById(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<Blog>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@Override
+	public ResponseEntity<String> deleteById(String id) {
+		try {
+			return blogService.deleteById(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
--- BLOG-Test/src/main/java/com/service/BlogService.java
+++ BLOG-Test/src/main/java/com/service/BlogService.java
@@ -1,11 +1,20 @@
 package com.service;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 
+import com.pojo.Blog;
+
 public interface BlogService {
 	
 	ResponseEntity<String> addBlog(Map<String, String> requestMap);
+	
+	ResponseEntity<List<Blog>> getAllBlog();
+	
+	ResponseEntity<Blog> getBlogById(String id);
+	
+	ResponseEntity<String> deleteById(String id);
 	
 }
